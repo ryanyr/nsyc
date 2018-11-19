43a3f9f49a93a453fa499e6e64fd50f5e34de11a
yuran <335023379@qq.com>
2018-11-19 11:44:16 +0800

    1
    11

--- temp/quick-scripts/assets/script/sample.js
+++ temp/quick-scripts/assets/script/sample.js
@@ -1,5 +1,5 @@
-"use strict";
-cc._RF.push(module, '420e5jUP/FPwJ9KQwbAkxd5', 'sample');
+(function() {"use strict";var __module = CC_EDITOR ? module : {exports:{}};var __filename = 'preview-scripts/assets/script/sample.js';var __require = CC_EDITOR ? function (request) {return cc.require(request, require);} : function (request) {return cc.require(request, __filename);};function __define (exports, require, module) {"use strict";
+cc._RF.push(module, '420e5jUP/FPwJ9KQwbAkxd5', 'sample', __filename);
 // script/sample.js
 
 'use strict';
@@ -85,3 +85,15 @@ cc.Class({
 );
 
 cc._RF.pop();
+        }
+        if (CC_EDITOR) {
+            __define(__module.exports, __require, __module);
+        }
+        else {
+            cc.registerModuleFunc(__filename, function () {
+                __define(__module.exports, __require, __module);
+            });
+        }
+        })();
+        //# sourceMappingURL=sample.js.map
+        
